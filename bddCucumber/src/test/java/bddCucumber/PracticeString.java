3e2da47febfc1086eb4199ff7068152970360fba
Subhan <subhan@MacBook-Pro.fios-router.home>
2021-10-12 23:45:19 -0400

    first commit

---
+++ bddCucumber/src/test/java/bddCucumber/PracticeString.java
@@ -0,0 +1,25 @@
+package bddCucumber;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PracticeString {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver",
+                "/Users/subhan/Downloads/geckodriver2");
+
+      
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.target.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       
+        
+        driver.findElement(By.xpath("//*[@id=\"divLogedOutScreen\"]/div/div[1]/div[1]/div/div[2]/div")).click();
+
+	}
+
+}
